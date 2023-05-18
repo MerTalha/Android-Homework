f577d3b1b0b578bc2320b1b9dfe224fe197023e8
MerTalha <merttalhasimsek@gmail.com>
2023-05-18 20:37:06 +0300

    Final-Update

--- week-13/app/src/main/java/com/example/week13/MainActivity.java
+++ week-13/app/src/main/java/com/example/week13/MainActivity.java
@@ -1,18 +1,16 @@
 package com.example.week13;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.week13.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     LocalDataManager localDataManager;
+    String locationText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,33 +18,30 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         localDataManager = new LocalDataManager();
-
-
-    }
-    Button button = (Button) findViewById(R.id.saveBtn);
-    void F_SetData() {
-        button.setOnClickListener(new View.OnClickListener() {
+        binding.saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!binding.infoView.getText().equals(""))
-                    localDataManager.setSharedPreference(getApplicationContext(), "name", binding.infoView.getText().toString());
+                    localDataManager.setSharedPreference(getApplicationContext(), "name", binding.nameTxt.getText().toString());
+                    localDataManager.setSharedPreference(getApplicationContext(), "location", binding.locationTxt.getText().toString());
+                    localDataManager.setSharedPreference(getApplicationContext(), "year", binding.yearTxt.getText().toString());
+            }
+        });
+        binding.showBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String isim = localDataManager.getSharedPreference(getApplicationContext(), "name", "");
+                String location = localDataManager.getSharedPreference(getApplicationContext(), "location", "");
+                String year = localDataManager.getSharedPreference(getApplicationContext(), "year","");
+                String text = isim + " " + location + " " + year;
+                binding.infoView.setText(text);
+            }
+        });
+        binding.deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                localDataManager.clearSharedPreference(getApplicationContext());
+                binding.infoView.setText("Lokasyon, isim ve yıl burada gözükür./Başarıyla siilindi.");
             }
         });
     }
-
-    /*@OnClick(R.id.showBtn)
-    void F_GetData() {
-        String isim = localDataManager.getSharedPreference(getApplicationContext(), "name", "");
-        Toast.makeText(this, isim, Toast.LENGTH_LONG).show();
-    }
-
-    @OnClick(R.id.deleteBtn)
-    void F_ClearData() {
-        localDataManager.clearSharedPreference(getApplicationContext());
-    }
-
-    @OnClick(R.id.deleteBtn)
-    void F_RemoveData() {
-        localDataManager.removeSharedPreference(getApplicationContext(), "name");
-    }*/
 }
